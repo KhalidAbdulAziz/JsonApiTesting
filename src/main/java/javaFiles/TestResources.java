56e227e692a27477606bc7b8086ecfe6ddd94971
Khalid Abdul Aziz <khalidaziz9011@gmail.com>
2020-01-06 17:02:00 +0500

    First Commit

---
+++ src/main/java/javaFiles/TestResources.java
@@ -0,0 +1,15 @@
+package javaFiles;
+
+public class TestResources {
+    public static String placePostData()
+    {
+        String res = "/maps/api/place/add/json";
+        return res;
+    }
+
+    public static String deletePostData()
+    {
+        String res_del = "/maps/api/place/delete/json";
+        return res_del;
+    }
+}
